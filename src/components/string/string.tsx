54c988e5a23bf00f0e6f8351ab8c1e6848e9afb9
Nikita Batalov <stereoslayer@gmail.com>
2023-10-12 01:11:39 +0300

    initial commit

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+        <Input/>
     </SolutionLayout>
   );
 };
